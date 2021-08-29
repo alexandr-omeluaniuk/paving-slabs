5d7c29004aae9b2a43396edf204f3e520b03417d
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-08-29 10:39:51 +0300

    CAD grid.

--- src/lib/cad/index.js
+++ src/lib/cad/index.js
@@ -19,11 +19,19 @@ export class CAD {
         this.scale = initialScale;
         this.width = width;
         this.height = height;
+        this.centerX = 0;
+        this.centerY = 0;
         this.stage = new Konva.Stage({
             container: containerId,
             width: width,
             height: height,
             draggable: true
+        });
+        this.stage.on('dragmove', (e) => {
+            const coords = e.target.getPointerPosition();
+            this.centerX = coords.x;
+            this.centerY = coords.y;
+            this._renderGrid();
         });
         this.mainLayer = new Konva.Layer();
         this.stage.add(this.mainLayer);
@@ -37,19 +45,24 @@ export class CAD {
         const GRID_LINE_MAIN_WIDTH = .2;
         const GRID_LINE_AUX = .1;
         const SHIFT = 10;
-        var grid = new Konva.Group({
-            x: 0,
-            y: 0
-        });
+        if (!this.grid) {
+            this.grid = new Konva.Group({
+                x: 0,
+                y: 0
+            });
+            this.mainLayer.add(this.grid);
+        } else {
+            this.grid.removeChildren();
+        }
         for (let i = SHIFT; i < this.height; i += STEP) {
-            grid.add(new Konva.Line({
+            this.grid.add(new Konva.Line({
                 points: [0, i, this.width, i],
                 stroke: 'blue',
                 strokeWidth: GRID_LINE_MAIN_WIDTH,
                 lineCap: 'round',
                 lineJoin: 'round'
             }));
-            grid.add(new Konva.Text({
+            this.grid.add(new Konva.Text({
                 x: 0,
                 y: i + 2,
                 text: (i - SHIFT) / 100,
@@ -58,7 +71,7 @@ export class CAD {
                 fill: 'blue'
             }));
             for (let j = i; j < i + STEP; j += STEP_AUX) {
-                grid.add(new Konva.Line({
+                this.grid.add(new Konva.Line({
                     points: [0, j, this.width, j],
                     stroke: 'green',
                     strokeWidth: GRID_LINE_AUX,
@@ -68,23 +81,23 @@ export class CAD {
             }
         }
         for (let i = SHIFT; i < this.width; i += STEP) {
-            grid.add(new Konva.Line({
+            this.grid.add(new Konva.Line({
                 points: [i, 0, i, this.height],
                 stroke: 'blue',
                 strokeWidth: GRID_LINE_MAIN_WIDTH,
                 lineCap: 'round',
                 lineJoin: 'round'
             }));
-            grid.add(new Konva.Text({
+            this.grid.add(new Konva.Text({
                 x: i + 2,
                 y: 0,
                 text: (i - SHIFT) / 100,
                 fontSize: 10,
-                fontFamily: 'Calibri',
+                fontFamily: 'Roboto,Calibri',
                 fill: 'blue'
             }));
             for (let j = i; j < i + STEP && j < this.width; j += STEP_AUX) {
-                grid.add(new Konva.Line({
+                this.grid.add(new Konva.Line({
                     points: [j, 0, j, this.height],
                     stroke: 'green',
                     strokeWidth: GRID_LINE_AUX,
@@ -93,6 +106,5 @@ export class CAD {
                 }));
             }
         }
-        this.mainLayer.add(grid);
     }
 }
